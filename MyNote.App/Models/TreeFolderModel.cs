3f79982290f581e6894ff2a3cb77fc1e242384cb
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2026-07-22 21:03:21 +0300

    Черновой вариант накинул

---
+++ MyNote.App/Models/TreeFolderModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MyNote.App.Models;
+
+public class TreeFolderModel
+{
+    public string Name { get; set; }
+    public ObservableCollection<TreeFolderModel> Children { get; } = [];
+}
--- MyNote.App/ViewModels/MainViewModel.cs
+++ MyNote.App/ViewModels/MainViewModel.cs
@@ -1,11 +1,42 @@
+using System.Collections.ObjectModel;
 using Avalonia.Platform.Storage;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MyNote.App.Models;
 
 namespace MyNote.App.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
+
+    public MainViewModel()
+    {
+        #region DEBUG
+        IsFolderSelected = true;
+        SelectedFolderPath = @"C:\Users\Alex\Documents\TestValue";
+        #endregion
+    }
+
+    [ObservableProperty]
+    private ObservableCollection<TreeFolderModel> tree = new()
+    {
+        new TreeFolderModel
+        {
+            Name = "First",
+            Children =
+            {
+                new TreeFolderModel
+                {
+                    Name = "Second"
+                },
+                new TreeFolderModel
+                {
+                    Name = "Three"
+                }
+            }
+        }
+    };
+
     [ObservableProperty]
     public partial bool IsFolderSelected { get; set; }
 
